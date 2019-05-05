4cd240978168ca7734e218b0f114ca9b7501f86e
Simon Biggs <me@simonbiggs.net>
2019-05-05 17:36:56 +1000

    overwrite deploy on webworker also

--- netlify-command.sh
+++ netlify-command.sh
@@ -1,3 +1,3 @@
 wget -q https://github.com/iodide-project/pyodide/releases/download/0.12.0/pyodide-build-0.12.0.tar.bz2
 tar xjC assets -f pyodide*.tar.bz2
-cp pyodide.js assets/pyodide.js
+cp overwrites/* assets/
