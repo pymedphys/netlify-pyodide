05da8d77f4391a4fe63bd7a849464cdac8c26b38
Simon Biggs <me@simonbiggs.net>
2019-05-05 17:08:36 +1000

    attempt install

--- netlify-command.sh
+++ netlify-command.sh
@@ -1,3 +1,5 @@
+apt-get install wget tar
+
 wget https://github.com/iodide-project/pyodide/releases/download/0.12.0/pyodide-build-0.12.0.tar.bz2
 tar xjC assets -f pyodide*.tar.bz2
 cp pyodide.js assets/pyodide.js
